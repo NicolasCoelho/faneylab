dcc6e0328bf5d82d0be2aa8d85ae3943b85f49f5
NicolasCoelho <nicolas_mansan@hotmail.com>
2021-10-02 15:31:58 -0300

    English translation prepared to others languages

---
+++ translations/en.js
@@ -0,0 +1,75 @@
+module.exports = {
+    metaKeywords: "Digital Agency, We help your business to grow, Let our experts bring success business, Boost your brand with these key features, Business Plan Services, Contact Us",
+    pageDescription: "Digital Agency with fair price to expand your business. Social media management and web site development.",
+    menu: {
+        page1: "Home",
+        page2: "Social Media",
+        page3: "Services",
+        page4: "Website",
+        page5: "Contact Us"
+    },
+    section1: {
+        preTitle: "About us",
+        text: "Helping companies around the world to expand their revenues by a digital presence with fair price.",
+        actionBtn: "Read more"
+    },
+    section2: {
+        title: "We help your business to grow",
+        text: "Yes, growing your business is more than possible!&nbsp;<br>With our help, your company's digital presence will give you a<br><strong>professional</strong>&nbsp;appearence.",
+        actionBtn: "Read More",
+        commas: "Our team comes to your rescue in order to help you cope with this state of confusion. We take care of everything that you need to build an unshakable and strong presence for your brand in this new digital world."
+    },
+    section3: {
+        title: "Let our experts bring success business",
+        point1: "strategy",
+        point2: "marketing",
+        point3: "Design",
+        text: "We are specialized in creating digital presence for our costumers. We believe a good first impression is essential to anyone who's bringing their work to social media. Our experts have been trained to deliver the best in these three pillars."
+    },
+    section4: {
+        title: "Instagram and Facebook",
+        text: "Together, these social medias have 4 bilions active users. They have an incredible power to expose your business to the world. We got the strategy and the best tools to manage it.",
+        actionBtn: "Read More"
+    },
+    section5: {
+        preTitle: "FEATURES",
+        title: "Boost your brand with these key features",
+        point1: {
+            title: "Social Media",
+            text: "Instagram and Facebook creation and management."
+        },
+        point2: {
+            title: "Web Site",
+            text: "Development with the best tools"
+        },
+        point3: {
+            title: "Marketing",
+            text: "We can help you to boost engagement with your visitors by ensuring that people have access to the information that they want"
+        }
+    },
+    section6: {
+        title: "Web site Development",
+        text: "Imagine having your own website. Surely our developers could create and maintain your services running with the modern tools on the market."
+    },
+    section7: {
+        title: "Business Plan Services",
+        text: "We are sure that we got the right plan for you for the fairly price in the market. Contact us to get a free buget.",
+        actionBtn: "Contact us"
+    },
+    section8: {
+        title: "Contact Us",
+        form: {
+            name: "Enter your Name",
+            email: "Enter a valid email address",
+            message: "Enter tour message",
+            submitBtn: "Submit",
+            sentMessage: "Thank you! Your message has been sent.",
+            failMessage: "Unable to send your message. Please fix errors then try again."
+        },
+        contact: {
+            phone: "Call Us on whats app",
+            email: "Email",
+            businessHours: "Business Hours"
+        }
+    }
+}
---
+++ translations/pt.js
@@ -0,0 +1,11 @@
+module.exports = {
+    metaKeywords: "Digital Agency, We help your business to grow, Let our experts bring success business, Boost your brand with these key features, Business Plan Services, Contact Us",
+    pageDescription: "Digital Agency with fair price to expand your business. Social media management and web site development.",
+    menu: {
+        page1: "Início",
+        page2: "Social Media",
+        page3: "Serviços",
+        page4: "Sites",
+        page5: "Contato"
+    }
+}
